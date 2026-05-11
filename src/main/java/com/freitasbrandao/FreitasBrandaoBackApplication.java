72348a2447e1c4c280d9e32ba7d5d29a712b593f
Gabriel Marques Fiel <Gabriel.m.fiel@gmail.com>
2026-05-11 15:42:25 -0300

    Commit inicial
    Adição da estrutura de pastas do projeta

---
+++ src/main/java/com/freitasbrandao/FreitasBrandaoBackApplication.java
@@ -0,0 +1,13 @@
+package com.freitasbrandao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreitasBrandaoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreitasBrandaoBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/freitasbrandao/FreitasBrandaoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freitasbrandao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreitasBrandaoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
